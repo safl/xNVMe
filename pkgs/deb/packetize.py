cf68bf4e4580caf3b39f070a166644fddadb8e7a
Simon A. F. Lund <simon.lund@samsung.com>
2023-11-16 15:55:45 +0100

    feat(debian): add pkg-files and script for packaging
    TODO: split the package into: libxnvme-dev, and xnvme (tools)
    
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- pkgs/deb/packetize.py
+++ pkgs/deb/packetize.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+These are notes on what to do when producing a Debian source package. The notes
+are taken as a self-contained Python script using only included-batteries, such
+that it can be conveniently dropped in where needed, such as a GitHUB Action
+pipeline.
+"""
 import logging as log
 import argparse
 import urllib.request
@@ -16,14 +22,10 @@ log.basicConfig(
 
 
 def download_github_asset(args):
-    """Download"""
+    """Downloads the github asset, unless a local file with the same filename exists"""
 
-    src_filename = f"{args.project_name.lower()}-{args.project_version}.tar.gz"
-    src_filepath = Path().cwd() / src_filename
-    orig_filepath = src_filepath.with_name(f"{args.project_name.lower()}_{args.project_version}.orig.tar.gz")
-
-    if src_filepath.exists():
-        log.info(f"'{src_filepath}' exists; skipping download")
+    if args.src_filepath.exists():
+        log.info(f"'{args.src_filepath}' exists; skipping download")
         return
 
     url = "/".join(
@@ -34,18 +36,18 @@ def download_github_asset(args):
             "releases",
             "download",
             f"v{args.project_version}",
-            src_filename,
+            args.src_filename,
         ]
     )
 
     with urllib.request.urlopen(url) as response:
-        with src_filepath.open("wb") as content:
+        with args.src_filepath.open("wb") as content:
             content.write(response.read())
 
-    run(["cp", f"{src_filepath}", ])
-    shutil.copy
 
 def parse_args():
+    """Parse command-line arguments and derive other infot"""
+
     parser = argparse.ArgumentParser(
         description="Create package using release from tarball from GitHUB"
     )
@@ -58,14 +60,53 @@ def parse_args():
     )
     parser.add_argument("--project-version", required=True, help="The project version")
     parser.add_argument(
-        "--output", default=Path.cwd(), type=Path, help="Directory to store it in"
+        "--output",
+        default=Path.cwd() / "builddir",
+        type=Path,
+        help="Directory to store it in",
     )
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    # The local filename of the source-archive
+    args.src_filename = f"{args.project_name.lower()}-{args.project_version}.tar.gz"
+    args.src_filepath = args.output / args.src_filename
+
+    # This is the root of source archive; after it is extracted
+    args.src_root = args.src_filepath.with_name(
+        f"{args.project_name.lower()}-{args.project_version}"
+    )
+
+    # This is an archive-file which is needed by the debian packaging magic
+    args.orig_filename = (
+        f"{args.project_name.lower()}_{args.project_version}.orig.tar.gz"
+    )
+    args.orig_filepath = args.src_filepath.with_name(args.orig_filename)
+
+    return args
 
 
 def main(args):
+    """Script retrieving xNVMe source archive and building the debian source package"""
+
+    run(["rm", "-r", f"{args.output}"])
+
+    args.output.mkdir(parents=True, exist_ok=True)
+
+    # Grab the "released" source-archive
     download_github_asset(args)
+
+    # Create the orig.tar.gz
+    run(["cp", f"{args.src_filepath}", f"{args.orig_filepath}"])
+
+    # Extract the source
+    run(["tar", "-xzf", f"{args.src_filepath}"], cwd=args.output)
+
+    # Inject the 'debian' folder
+    run(["cp", "-r", "debian", f"{args.src_root}"])
+
+    # Now, go ahead and build the package
+    run(["dpkg-buildpackage", "-us", "-uc"], cwd=args.src_root)
 
 
 if __name__ == "__main__":
